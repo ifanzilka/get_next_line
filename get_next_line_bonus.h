5d17fe596f623348ffd4c857f8a3afac692b958d
Blood Marillion <bmarilli@ev-m6.kzn.21-school.ru>
2020-11-10 22:40:44 +0300

    init

---
+++ get_next_line_bonus.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bmarilli <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/10 15:52:32 by bmarilli          #+#    #+#             */
+/*   Updated: 2020/11/10 15:52:41 by bmarilli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+
+size_t	ft_strlen(const char *str);
+char	*ft_strdup(const char *s1);
+int		get_next_line(int fd, char **line);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+#endif
